5532701b5e7eaf320059dd454030c51f8e81a7c6
Michal Kielak <m.kielak@stud.elka.pw.edu.pl>
2013-12-26 14:43:24 +0100

    Adding edges functionality

--- graphicNode.py
+++ graphicNode.py
@@ -1,9 +1,13 @@
 from PySide import QtGui
+from PySide import QtCore
 
 class GraphicNode(QtGui.QGraphicsEllipseItem):
     def __init__(self, id, x, y):
-        self.radius = 8
+        self.radius = 24
         self.id = id
+        self.x = x
+        self.y = y
         super(GraphicNode, self).__init__(x - self.radius / 2, y - self.radius / 2, self.radius, self.radius)
         
-    
+    def scenePos(self):
+        return QtCore.QPointF(self.x, self.y)
--- view.py
+++ view.py
@@ -1,4 +1,4 @@
-from PySide import QtGui
+from PySide import QtGui, QtCore
 from layout.main_window import Ui_MainWindow
 from graphicNode import GraphicNode
 from model import GraphModel
@@ -16,10 +16,43 @@ class View(QtGui.QMainWindow):
         self.scene = QtGui.QGraphicsScene()
         self.ui.graphicsView.setScene(self.scene)
         self.scene.mousePressEvent = self.ownMousePressEvent
+        self.scene.mouseReleaseEvent = self.ownMouseReleaseEvent
+        self.scene.mouseMoveEvent = self.ownMouseMoveEvent
+        self.clickedNodeId = None
+        self.currentEdge = None
+        self.currentEdgeStart = None
 
     def ownMousePressEvent(self, event):
-        node_id = self.graph.nextId()
-        node = GraphicNode(node_id, event.scenePos().x(), event.scenePos().y())
-        print event.scenePos().x(), event.scenePos().y()
-        self.scene.addItem(node)
-        self.graph.add_node(node_id)
+        if not self.scene.itemAt(event.scenePos()):
+            node_id = self.graph.nextId()
+            node = GraphicNode(node_id, event.scenePos().x(), event.scenePos().y())
+            self.scene.addItem(node)
+            self.graph.add_node(node_id)
+        else:
+            clickedNode = self.scene.itemAt(event.scenePos())
+            self.clickedNodeId = clickedNode.id
+            self.currentEdgeStart = clickedNode.scenePos()
+            self.currentEdge = QtGui.QGraphicsLineItem(clickedNode.scenePos().x(), clickedNode.scenePos().y(), event.scenePos().x(), event.scenePos().y())
+            self.currentEdge.setZValue(-1)
+            pen = QtGui.QPen(QtGui.QBrush(QtGui.QColor(0,0,0,255)), 4)
+            self.currentEdge.setPen(pen)
+            self.scene.addItem(self.currentEdge)
+    
+    def ownMouseReleaseEvent(self, event):
+        if self.scene.itemAt(event.scenePos()) and self.currentEdge and isinstance(self.scene.itemAt(event.scenePos()), GraphicNode):
+            clickedNode = self.scene.itemAt(event.scenePos())
+            self.graph.add_edge(self.clickedNodeId, clickedNode.id)
+            self.currentEdge.setLine(self.currentEdgeStart.x(), self.currentEdgeStart.y(), clickedNode.scenePos().x(), clickedNode.scenePos().y())
+            self.currentEdge = None
+            self.clickedNodeId = None
+            self.currentEdgeStart = None
+        elif self.currentEdge:
+            self.scene.removeItem(self.currentEdge)
+            self.currentEdge = None
+            self.clickedNodeId = None
+            self.currentEdgeStart = None
+        
+    def ownMouseMoveEvent(self, event):
+        if self.currentEdge:
+            self.currentEdge.setLine(self.currentEdgeStart.x(), self.currentEdgeStart.y(), event.scenePos().x(), event.scenePos().y())
+
